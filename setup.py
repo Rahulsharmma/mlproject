0736b83c5d387a4432c508168c991fd151422a31
Rahulsharmma <goldy.rahul.sharma@gmail.com>
2024-08-29 10:32:09 -0700

    initial setup is done

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hypen_e_dot='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the requirements 
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+
+    return requirements
+
+
+setup(
+    name ='mlproject',
+    author='Rahul Sharma',
+    version='0.0.1',
+    author_email='goldy.rahul.sharma@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
